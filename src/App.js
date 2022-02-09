d606649f253a577be8b8c6df26846b4293974b9b
nada bouhlel <nada.bouhlel0@gmail.com>
2022-02-09 16:54:35 +0100

    initial commitv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import react from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="spinner-border text-primary" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
     </div>
   );
 }
